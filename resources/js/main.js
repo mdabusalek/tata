58aadb331edf2c13a2f05115ccb256288b35ef66
salek <bhairabbari330@gmail.com>
2020-11-03 07:53:34 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var mixer = mixitup('.container');
+});
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
